3b62cb9e7d6f56549b1db3b79b87fb9f43727693
Benita1501 <benichris94@gmail.com>
2023-04-08 12:16:56 +0530

    taskdonemaster

---
+++ src/org/maths/Calculation.java
@@ -0,0 +1,16 @@
+package org.maths;
+
+public class Calculation {
+	
+	public void add() {
+		// TODO Auto-generated method stub
+System.out.println("Addition");
+	}
+	public static void main(String[] args) {
+		
+	Calculation c= new Calculation();
+	c.add();
+		
+	}
+
+}
